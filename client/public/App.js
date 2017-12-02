d0ab7e35e01f78ed2f9ca6492ec45d59205ef0f0
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-12-02 14:24:05 -0800

    Map the rendering pad function with this.row number of times. #4

--- client/public/App.js
+++ client/public/App.js
@@ -1,26 +1,18 @@
 import React, { Component } from 'react'
 import Pads from './Pads'
 
-const sound1 = new Audio('/Users/hoshikitsunoda/react-drum-machine/server/public/sounds/synth/C.mp3')
-
 class App extends Component {
   constructor(props) {
     super(props)
     this.list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
     this.row = [1,2,3,4,5,6,7,8,9,10,11,12]
-    this.sound = [sound1]
     this.renderRows = this.renderRows.bind(this)
-    this.playNote = this.playNote.bind(this)
-  }
-
-  playNote(sound) {
-    return sound.play()
   }
 
   renderRows() {
     return (
-      <div className="paddies">
-        {this.list.map((number, index) => <button onClick={this.playNote} key={index}></button>)}
+      <div className="rows">
+        {this.list.map((number, index) => <button key={index}></button>)}
       </div>
     )
   }
@@ -28,8 +20,7 @@ class App extends Component {
   render() {
     return (
       <div id="table">
-        <Pads renderRows={this.renderRows} />
-        {/*this.row.map((num, index) => <Pads key={index} renderRows={this.renderRows} />)*/}
+        {this.row.map((id, index) => <Pads key={index} renderRows={this.renderRows} />)}
       </div>
     )
   }
