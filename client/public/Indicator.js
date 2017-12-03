ce73d57f6ad129cfad57cc453ce90b5f5a4d3df0
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-12-02 17:07:56 -0800

    Added unique ID to each indicator. #6

--- client/public/Indicator.js
+++ client/public/Indicator.js
@@ -4,7 +4,7 @@ const Indicator = () => {
   const notes = ['C', 'D', 'E', 'F', 'G', 'A', 'B', 'KD', 'SD', 'HH', 'CL', 'SH']
   return(
     <div>
-      {notes.map((id, index) => <a className="notes" key={index}>{id}</a>)}
+      {notes.map((id, index) => <a className="notes" key={index} id={id + '-note'}>{id}</a>)}
     </div>
   )
 }
