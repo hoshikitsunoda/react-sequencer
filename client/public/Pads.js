464ee507a58d19ea0502fe196db3d73c289aa6d4
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-12-02 16:29:49 -0800

    Add Indicator component to render function of Pads.js. #6

--- client/public/Pads.js
+++ client/public/Pads.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import Pad from './Pad'
+import Indicator from './Indicator'
 
 const Pads = () => {
   const row = [1,2,3,4,5,6,7,8,9,10,11,12]
   return (
-    <div id="table">
-      {row.map((id, index) => <Pad key={index} id={id} />)}
+    <div>
+      <div id="table">
+        {row.map((id, index) => <Pad key={index} id={id} />)}
+      </div>
+      <div id="indicator">
+        {row.map((id, index) => <Indicator key={index} id={id} />)}
+      </div>
     </div>
   )
 }
