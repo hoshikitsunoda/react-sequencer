bf7b884de056ca948e589b3cff9e8a2c364b1e1d
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-12-02 16:59:43 -0800

    Add text to each element to indicate which note they are. #6

--- client/public/Indicator.js
+++ client/public/Indicator.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 const Indicator = () => {
+  const notes = ['C', 'D', 'E', 'F', 'G', 'A', 'B', 'KD', 'SD', 'HH', 'CL', 'SH']
   return(
     <div>
-      <a className="notes"></a>
+      {notes.map((id, index) => <a className="notes" key={index}>{id}</a>)}
     </div>
   )
 }
