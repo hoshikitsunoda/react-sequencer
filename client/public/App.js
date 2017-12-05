ba2b0344c8fcb5b536628cf50e0a25ab3108fb9e
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-12-04 23:04:48 -0800

    Inside the clicked function, add play audio. #8

--- client/public/App.js
+++ client/public/App.js
@@ -2,6 +2,19 @@ import React, { Component } from 'react'
 import Pads from './Pads'
 import Indicator from './Indicator'
 
+var sound1 = new Audio('/Users/hoshikitsunoda/react-drum-machine/server/public/sounds/synth/C.mp3')
+var sound2 = new Audio('/Users/hoshikitsunoda/react-drum-machine/server/public/sounds/synth/D.mp3')
+var sound3 = new Audio('/Users/hoshikitsunoda/react-drum-machine/server/public/sounds/synth/E.mp3')
+var sound4 = new Audio('/Users/hoshikitsunoda/react-drum-machine/server/public/sounds/synth/F.mp3')
+var sound5 = new Audio('/Users/hoshikitsunoda/react-drum-machine/server/public/sounds/synth/G.mp3')
+var sound6 = new Audio('/Users/hoshikitsunoda/react-drum-machine/server/public/sounds/synth/A.mp3')
+var sound7 = new Audio('/Users/hoshikitsunoda/react-drum-machine/server/public/sounds/synth/B.mp3')
+var sound8 = new Audio('/Users/hoshikitsunoda/react-drum-machine/server/public/sounds/synth/KD.mp3')
+var sound9 = new Audio('/Users/hoshikitsunoda/react-drum-machine/server/public/sounds/synth/SD.mp3')
+var sound10 = new Audio('/Users/hoshikitsunoda/react-drum-machine/server/public/sounds/synth/HH.mp3')
+var sound11 = new Audio('/Users/hoshikitsunoda/react-drum-machine/server/public/sounds/synth/CL.mp3')
+var sound12 = new Audio('/Users/hoshikitsunoda/react-drum-machine/server/public/sounds/synth/SH.mp3')
+
 class App extends Component {
   constructor(props) {
     super(props)
@@ -9,7 +22,8 @@ class App extends Component {
   }
 
   clicked() {
-    console.log('click')
+    const audio = document.querySelectorAll('.notes')
+    play.audio()
   }
 
   render() {
