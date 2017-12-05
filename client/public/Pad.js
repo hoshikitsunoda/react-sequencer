d5193a5149af98219318a8537277e25944f126bc
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-12-05 11:59:49 -0800

    In constructor, add this state set to active: false. #10

--- client/public/Pad.js
+++ client/public/Pad.js
@@ -5,6 +5,7 @@ const list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
 class Pad extends Component {
   constructor(props) {
     super(props)
+    this.state = { active: false }
   }
 
   render() {
