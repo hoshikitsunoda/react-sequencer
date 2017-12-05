9f494765f6f037da2fd295fa805c0e4f820016b7
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-12-04 23:23:40 -0800

    Add onclick event on button element in Pad.js. #10

--- client/public/Pad.js
+++ client/public/Pad.js
@@ -4,7 +4,7 @@ const Pad = () => {
   const list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
   return (
     <div>
-      {list.map((id, index) => <button key={index}></button>)}
+      {list.map((id, index) => <button onClick={onClick} key={index}></button>)}
     </div>
   )
 }
