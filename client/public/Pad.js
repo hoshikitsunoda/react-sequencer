32134dc8dbb0e0d3d5636e1068c4510f00e4621a
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-12-05 11:54:15 -0800

    Add Component to import from React in Pad.js. #10

--- client/public/Pad.js
+++ client/public/Pad.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Pad = () => {
+const Pad = ({ onClick }) => {
   const list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+  const notes = ['C', 'D', 'E', 'F', 'G', 'A', 'B', 'KD', 'SD', 'HH', 'CL', 'SH']
   return (
     <div>
       {list.map((id, index) => <button onClick={onClick} key={index}></button>)}
