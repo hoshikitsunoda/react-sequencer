114050d7122d57611c6befa322d90ee33881b749
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-12-06 17:21:45 -0800

    Add render function and return 16 list element. #13

--- client/public/SequenceIndicator.js
+++ client/public/SequenceIndicator.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react'
 
+const list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+
 class SequenceIndicator extends Component {
   constructor(props) {
     super(props)
+  }
+
+  render() {
+    return (
+      <ul>
+        {list.map((id, index) => {
+          return <li key={index} className="sequence-indicator" id={id + 'th'}></li>
+        })}
+      </ul>
+    )
   }
 }
