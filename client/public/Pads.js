289ab0eb790e96db9e3f20f1c62cb942f4700ca4
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-12-07 13:39:31 -0800

    Changed Pad component to functional and Pads to class so it has better hierarchy. #16

--- client/public/Pads.js
+++ client/public/Pads.js
@@ -1,17 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Pad from './Pad'
 
-const Pads = ({ onClick }) => {
-  const row = ['c', 'd', 'e', 'f', 'g', 'a', 'b', 'kd', 'sd', 'hh', 'cl', 'sh']
-  return (
-    <div id="table">
-      {row.map((id, index) => {
-        return (<div className="rows" key={index} id={id + '-row'}>
-          <Pad key={index} id={id} />
-        </div>)
-      })}
-    </div>
-  )
+class Pads extends Component {
+  constructor(props) {
+    super(props)
+    this.handleBgColorChange = this.handleBgColorChange.bind(this)
+    this.state = {
+      activePads: {}
+    }
+  }
+
+  handleBgColorChange(id) {
+    let currentPads = this.state.activePads
+    currentPads[id] === 'selected'
+      ? currentPads[id] = null
+      : currentPads[id] = 'selected'
+    this.setState({ activePads: currentPads })
+  }
+
+  render() {
+    const row = ['c', 'd', 'e', 'f', 'g', 'a', 'b', 'kd', 'sd', 'hh', 'cl', 'sh']
+    return (
+      <div id="table">
+        {row.map((rowNum, index) => {
+          return (<div className="rows" key={rowNum} id={rowNum + '-row'}>
+            <Pad
+              row={rowNum}
+              activePads={this.state.activePads}
+              click={this.handleBgColorChange} />
+          </div>)
+        })}
+      </div>
+    )
+  }
 }
 
 export default Pads
