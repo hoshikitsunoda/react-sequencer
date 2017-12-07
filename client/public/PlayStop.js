26088b8dde1b1cc2b14d0cdc3e2367b23007bc2d
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-12-06 16:21:07 -0800

    Add togglePlay function and set state. #12

--- client/public/PlayStop.js
+++ client/public/PlayStop.js
@@ -3,6 +3,12 @@ import React, { Component } from 'react'
 class PlayStop extends Component {
   constructor(props) {
     super(props)
+    this.state = { class: 'stop' }
+  }
+
+  togglePlay() {
+    const currentState = this.state.class
+    this.setState({ class: !currentState })
   }
 
   render() {
