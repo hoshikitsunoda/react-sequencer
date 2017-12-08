01ceb0ffb7fa28a688728553c3931b9aa3f7feb5
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-12-08 15:33:31 -0800

    Tested blinking on Sequence Indicators. #16

--- client/public/PlayStop.js
+++ client/public/PlayStop.js
@@ -1,47 +1,79 @@
 import React, { Component } from 'react'
 import SequenceIndicator from './SequenceIndicator'
 
-var steps
+let steps
 
 class PlayStop extends Component {
   constructor(props) {
     super(props)
     this.state = {
       class: 'stop',
-      id: 'null',
-      started: false
+      status: {},
+      started: false,
+      position: 0
     }
-    this.togglePlay = this.togglePlay.bind(this)
+    // this.togglePlay = this.togglePlay.bind(this)
     this.start = this.start.bind(this)
   }
 
-  togglePlay() {
-    const currentState = this.state.class
-    this.setState({ class: !currentState })
-  }
+  // togglePlay() {
+  //   const currentState = this.state.class
+  //   this.setState({ class: !currentState })
+  // }
 
   start() {
+    const currentState = this.state.class
+    this.setState({ class: !currentState })
+
     if(this.state.started === false) {
       steps = setInterval(() => this.stepper(), 500)
-      this.setState({ id: 'null', started: true })
+      this.setState({ started: true })
     }
     else {
       clearInterval(steps)
-      this.setState({ id: 'null', started: false })
+      this.setState({ position: 1, started: false })
     }
   }
 
   stepper() {
-    const litUp = (this.state.id === 'lit') ? 'null' : 'lit'
-    this.setState({ id: litUp })
+    const currentPosition = this.state.position
+    currentPosition !== 16
+      ? this.setState({ status: this.state.status, position: currentPosition + 1 })
+      : this.setState({ status: this.state.status, position: 1 })
+    const indicator = document.querySelectorAll('.sequence-indicator')
+    for(let i = 0; i < currentPosition; i++) {
+      indicator[i].classList.add('lit')
+      setInterval(() => indicator[i].classList.remove('lit'), 500, false)
+    }
+
+    // let currentStatus = this.state.status
+    // const currentPosition = this.state.position
+    // currentStatus[id] === 'lit'
+    //   ? currentStatus[id] = null
+    //   : currentStatus[id] = 'lit'
+    // currentPosition < 16
+    //   ? this.setState({ status: currentStatus, position: currentPosition + 1 })
+    //   : this.setState({ status: currentStatus, position: 1 })
+
+    // const currentPosition = this.state.position
+    // const litUp = (this.state.status === 'lit') ? 'null' : 'lit'
+    // if(this.state.position < 16) {
+    //   this.setState({ status: litUp, position: currentPosition + 1 })
+    // }
+    // else {
+    //   this.setState({ status: litUp, position: 1 })
+    // }
   }
 
   render() {
-    console.log(this.state.started);
+    // console.log(this.state.position);
+    // console.log(this.state.status);
     return (
       <div>
         <SequenceIndicator
-          started={this.state.id}
+          position={this.state.position}
+          status={this.state.status}
+          click={this.start}
         />
         <button onClick={this.start} id="playbutton">
           <i className={this.state.class ? 'fa fa-play' : 'fa fa-stop'}></i>
