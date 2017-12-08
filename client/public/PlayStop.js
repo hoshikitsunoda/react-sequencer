089eee1c0f3c8a1c9cfb8055df1fc02b51b028f3
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-12-07 22:50:26 -0800

    Tested to see if blink works. #16

--- client/public/PlayStop.js
+++ client/public/PlayStop.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+import SequenceIndicator from './SequenceIndicator'
+
+var steps
 
 class PlayStop extends Component {
   constructor(props) {
     super(props)
-    this.state = { class: 'stop' }
+    this.state = {
+      class: 'stop',
+      id: 'null',
+      started: false
+    }
     this.togglePlay = this.togglePlay.bind(this)
+    this.start = this.start.bind(this)
   }
 
   togglePlay() {
@@ -12,10 +20,30 @@ class PlayStop extends Component {
     this.setState({ class: !currentState })
   }
 
+  start() {
+    if(this.state.started === false) {
+      steps = setInterval(() => this.stepper(), 500)
+      this.setState({ id: 'null', started: true })
+    }
+    else {
+      clearInterval(steps)
+      this.setState({ id: 'null', started: false })
+    }
+  }
+
+  stepper() {
+    const litUp = (this.state.id === 'lit') ? 'null' : 'lit'
+    this.setState({ id: litUp })
+  }
+
   render() {
+    console.log(this.state.started);
     return (
       <div>
-        <button onClick={this.togglePlay} id="playbutton">
+        <SequenceIndicator
+          started={this.state.id}
+        />
+        <button onClick={this.start} id="playbutton">
           <i className={this.state.class ? 'fa fa-play' : 'fa fa-stop'}></i>
         </button>
       </div>
