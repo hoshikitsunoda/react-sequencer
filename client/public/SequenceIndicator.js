cb5bb3867f13c5b80666a2141573c0af46cd02b4
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-12-08 15:32:49 -0800

    Changed id for each button to index number. #16

--- client/public/SequenceIndicator.js
+++ client/public/SequenceIndicator.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 
-const SequenceIndicator = ({ started }) => {
+const SequenceIndicator = ({ status, position, click }) => {
     const list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
     return (
       <div>
         {list.map((id, index) => {
-          return <button key={index} className="sequence-indicator" id={started}></button>
+          return <button
+            key={index}
+            className="sequence-indicator"
+            id={index}
+            onClick={() => click(`${id}`)}
+            ></button>
         })}
       </div>
     )
