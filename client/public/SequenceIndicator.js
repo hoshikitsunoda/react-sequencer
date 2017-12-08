b8d7916a9bdf0d7709fbf79f526c22b581fa13a8
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-12-07 22:49:32 -0800

    Changed SequenceIndicator to functional component so it becomes child of PlayStop class. #16

--- client/public/SequenceIndicator.js
+++ client/public/SequenceIndicator.js
@@ -1,21 +1,14 @@
 import React, { Component } from 'react'
 
-const list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
-
-class SequenceIndicator extends Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
+const SequenceIndicator = ({ started }) => {
+    const list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
     return (
       <div>
         {list.map((id, index) => {
-          return <button key={index} className="sequence-indicator" id={id + 'th'}></button>
+          return <button key={index} className="sequence-indicator" id={started}></button>
         })}
       </div>
     )
   }
-}
 
 export default SequenceIndicator
