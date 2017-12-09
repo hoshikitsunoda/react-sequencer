d87e34c9ebfbda67dcd9118ff0fee4252ba9e566
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-12-08 18:02:21 -0800

    Modified the stepper function to add class only when id matches to current position. #16

--- client/public/PlayStop.js
+++ client/public/PlayStop.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import SequenceIndicator from './SequenceIndicator'
 
 const stable = require('stable-timer')
-
 let steps
 
 class PlayStop extends Component {
@@ -15,35 +14,45 @@ class PlayStop extends Component {
       position: 0
     }
     this.start = this.start.bind(this)
+    this.stop = this.stop.bind(this)
   }
 
   start() {
+    console.log(steps);
+    this.setState({ started: true })
+    steps = setInterval(() => this.stepper(), 250)
+
     const currentState = this.state.class
     this.setState({ class: !currentState })
+  }
 
-    if(this.state.started === false) {
-      steps = setInterval(() => this.stepper(), 500)
-      this.setState({ started: true })
-    }
-    else {
-      clearInterval(steps)
-      this.setState({ position: 1, started: false })
-    }
+  stop() {
+    console.log(steps);
+    this.setState({
+      position: 0,
+      started: false
+    })
+    clearInterval(steps)
   }
 
   stepper() {
     const currentPosition = this.state.position
-    currentPosition !== 16
-      ? this.setState({ status: this.state.status, position: currentPosition + 1 })
-      : this.setState({ status: this.state.status, position: 1 })
-    const indicator = document.querySelectorAll('.sequence-indicator')
-    for(let i = 0; i < currentPosition; i++) {
-      indicator[i].classList.add('lit')
-      setInterval(() => indicator[i].classList.remove('lit'), 500, false)
+    currentPosition <= 15
+      ? this.setState({ status: true, position: currentPosition + 1 })
+      : this.setState({ position: 0 })
+    if(this.state.started === true) {
+      const indicator = document.querySelectorAll('.sequence-indicator')
+      for(let i = 0; i < indicator.length; i++) {
+        if(currentPosition.toString() === indicator[i].id) {
+          indicator[i].classList.add('lit')
+          setInterval(() => indicator[i].classList.remove('lit'), 250, false)
+        }
+      }
     }
   }
 
   render() {
+    console.log(this.state.started);
     return (
       <div>
         <SequenceIndicator
@@ -51,7 +60,7 @@ class PlayStop extends Component {
           status={this.state.status}
           click={this.start}
         />
-        <button onClick={this.start} id="playbutton">
+        <button onClick={this.state.started === false ? this.start : this.stop} id="playbutton">
           <i className={this.state.class ? 'fa fa-play' : 'fa fa-stop'}></i>
         </button>
       </div>
