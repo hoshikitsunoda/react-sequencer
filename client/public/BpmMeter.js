b720eaed0dffd12b51acf9a1565a16cb8816326a
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-12-19 16:55:19 -0800

    Change bpmmeter element to input. #24

--- client/public/BpmMeter.js
+++ client/public/BpmMeter.js
@@ -2,7 +2,13 @@ import React, { Component } from 'react'
 
 class BpmMeter extends Component {
   render() {
-    
+    return (
+      <div id="bpmmeter">
+        <form>
+          <input type="text" value="120.00"/>
+        </form>
+      </div>
+    )
   }
 }
 
